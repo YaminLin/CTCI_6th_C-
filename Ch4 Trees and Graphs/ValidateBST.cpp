a905c23ef81df4e0dcf098914c3567e4277ab135
YaminLin <YaminLin@users.noreply.github.com>
2018-06-11 00:02:00 -0400

    add ValidateBST

---
+++ Ch4 Trees and Graphs/ValidateBST.cpp
@@ -0,0 +1,22 @@
+#include "Ch4_TreesAndGraphs.h"
+bool _checkbst(node *root, long left, long right);
+
+bool ValidateBST(node* root) {
+	if (root == NULL)
+		return true;
+
+	return (_checkbst(root->left, (long)INT_MIN - 1, (long)root->key) && _checkbst(root->right, (long)root->key, (long)INT_MAX + 1));
+}
+
+
+bool _checkbst(node* root, long left, long right)
+{
+
+	if (root == NULL)
+		return true;
+
+	if (left < root->key &&  root->key < right)
+		return (_checkbst(root->left, left, root->key) && _checkbst(root->right, root->key, right));
+	else
+		return false;
+}
